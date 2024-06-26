fbc04cc6338f5210523eb01b96207bc721343752
Vaibhav Chaudhary <vaibhav.dvs@gmail.com>
2024-06-26 15:02:16 +0530

    section3

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { IntroComponent } from './intro/intro.component';
+import { Section1Component } from './section1/section1.component';
+import { Section2Component } from './section2/section2.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [IntroComponent],
+  imports: [IntroComponent, Section1Component, Section2Component],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
