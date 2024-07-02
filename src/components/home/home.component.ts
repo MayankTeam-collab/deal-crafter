203edb2ae4e0c3ecfbcc36b063cc81793138406d
Vaibhav Chaudhary <vaibhav.dvs@gmail.com>
2024-07-02 16:51:57 +0530

    Renamed components

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { IntroComponent } from './intro/intro.component';
-import { Section1Component } from './section1/section1.component';
-import { Section2Component } from './section2/section2.component';
-import { Section3Component } from './section3/section3.component';
+import { SalesEnableComponent } from './sales-enable/sales-enable.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { OurWorkComponent } from './our-work/our-work.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [IntroComponent, Section1Component, Section2Component, Section3Component],
+  imports: [IntroComponent, SalesEnableComponent, AboutUsComponent, OurWorkComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
---
+++ src/components/home/sales-enable/sales-enable.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-enable',
+  standalone: true,
+  imports: [],
+  templateUrl: './sales-enable.component.html',
+  styleUrl: './sales-enable.component.scss'
+})
+export class SalesEnableComponent {
+
+}
